37ddf51d6ccbac92e042bf01d0620b528f61521f
David Schmidt <Lavicola7777@t-online.de>
2020-07-03 22:17:08 +0200

    Update usb_host.c
    if  File does not exist create it else append the existing file. No other cases supportet

--- Src/usb_host.c
+++ Src/usb_host.c
@@ -13,7 +13,7 @@
   * This software component is licensed by ST under Ultimate Liberty license
   * SLA0044, the "License"; You may not use this file except in compliance with
   * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
+  *   s                          www.st.com/SLA0044
   *
   ******************************************************************************
   */
@@ -25,7 +25,7 @@
 #include "usbh_core.h"
 #include "usbh_msc.h"
 
-/* ssUSER CODE BEGIN Includes */
+/* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
 
@@ -46,9 +46,14 @@ FRESULT res;
 uint32_t bytesWritten;
 uint8_t rtext[200];
 uint8_t wtext[] = "USB Host Library : Mass Storage Example";
-uint8_t name[10]="test";//name of the file
+uint8_t name[30]="awesome.txt";//name of the file
 uint16_t counter=0;
 uint32_t i=0;
+uint8_t isMounted = 0;
+FILINFO fno;
+
+
+	
 static int32_t uart_length=0;
 extern char USBHPath [];  /* USBH logical drive path */
 
@@ -58,95 +63,43 @@ uint8_t uart_tx_buffer[100];
 
 void userFunction(void) {
 	UINT bytesread;
-	if (Appli_state == APPLICATION_READY) {
-			i=0;
-			// Begin TODO 3 MSC_HOST_HANDS_ON: Complete the code skeleton
-			//Code Skeleton
-#if 1
-			// mount it first?
-			uint8_t a = f_mount(&USBH_fatfs,"0://",1);
-		
-			//File name creation
-//			sprintf("1.txr","0://",counter++);
+	FRESULT l_result = FR_DISK_ERR;
 	
+	
+	if (Appli_state == APPLICATION_READY) {		
+		// mount it first!
+		if(isMounted == 0)
+		{
+		uint8_t a = f_mount(&USBH_fatfs,"0://",1);
+			isMounted = 1;
+		}
 		
-			/*Create a file*/
-			if (f_open(&MyFile,"1.txt",FA_CREATE_ALWAYS |FA_WRITE) != FR_OK ) {
-				/* Creation failed */
-				uart_length=sprintf(uart_tx_buffer, "Cannot open %s file \n", name);
-				HAL_UART_Transmit(&huart3, uart_tx_buffer, (uint16_t)uart_length,1000);
-
-			} else {
-				uart_length=sprintf(uart_tx_buffer, "file %s created \n", name);
-				HAL_UART_Transmit(&huart3, uart_tx_buffer, (uint16_t)uart_length,1000);
-				/*write message to the file. Use variable wtext, bytesWritten*/
-				res =f_write(&MyFile,wtext,sizeof(wtext),&bytesWritten);
-
-				/*close the file*/
-				f_close(&MyFile);
-
-				/*check number of written bytes*/
-				if ((bytesWritten == 0) || (res != FR_OK)) {
-					/*error during writing*/
-					uart_length=sprintf(uart_tx_buffer, "write error \n");
-					HAL_UART_Transmit(&huart3, uart_tx_buffer,(uint16_t)uart_length, 1000);
-				}
-				else {
-					/*open file to verification*/
-					if (f_open(&MyFile,name,FA_READ) != FR_OK ) {
-						/*file open failure*/
-						uart_length=sprintf(uart_tx_buffer,"Cannot open %s file for verify \n", name);
-						HAL_UART_Transmit(&huart3, uart_tx_buffer,(uint16_t)uart_length, 1000);
-					} else {
-						/*Read file content. Use variable : rtext, bytesread*/
-						res= f_read(&MyFile,rtext,sizeof(rtext), &bytesread);
-
-						if ((bytesread == 0) || (res != FR_OK)) {
-							/*read fail*/
-							uart_length=sprintf(uart_tx_buffer,"Cannot read file for verification \n");
-							HAL_UART_Transmit(&huart3, uart_tx_buffer,(uint16_t)uart_length, 1000);
-						} else {
-							/*read success*/
-						}
-
-						/*close the file*/
-						if (f_close(&MyFile) != FR_OK) {
-							/*check number of written bytes*/
-							uart_length=sprintf(uart_tx_buffer, "fclose fail \n");
-							HAL_UART_Transmit(&huart3, uart_tx_buffer,(uint16_t)uart_length, 1000);
-							int a = 0;
-							while (1){
+		
+		switch(f_stat(name,&fno)){
+			
+			case FR_NO_FILE:
+				l_result = f_open(&MyFile,name,FA_CREATE_NEW|FA_WRITE);
+			break;
+			case FR_OK:
+				l_result = f_open(&MyFile,name,FA_OPEN_APPEND|FA_WRITE);
+			break;
+			// I don´t expect other errors
+			default:
+				l_result = FR_DISK_ERR;
+		}
+		
+			if(l_result == FR_OK){
 				
-								a=5;
-							
-							}
-						}
-					}
-					/* Compare read data with the expected data */
-					if ((bytesread == bytesWritten)) {
-						/*verification success full - number of written bytes is equal to number of read bytes*/
-						uart_length=sprintf(uart_tx_buffer,"verification OK - read number of bytes is equal to written number of bytes \n");
-						HAL_UART_Transmit(&huart3, uart_tx_buffer,((uint16_t)uart_length), 5000);
-
-					} else {
-						/*verification failed - number of written bytes is not equal to number of read bytes*/
-						uart_length=sprintf(uart_tx_buffer, "verify fail \n");
-						HAL_UART_Transmit(&huart3, uart_tx_buffer,(uint16_t)uart_length, 1000);
-					}
-					/*end program execution after verification*/
-				}
+				HAL_UART_Transmit(&huart3, "This is a test", strlen("This is a test"),1000);
+				f_puts("This is a test\n",&MyFile);	
+				f_close(&MyFile);
+				
 			}
-#endif
-	
-		i++;
+		
 
-	// End MSC_HOST_HANDS_ON: Add the call to userFunction()
 
   }
 }
-
-/* USER CODE END 0 */
-
 
 /*
  * -- Insert your variables declaration here --
